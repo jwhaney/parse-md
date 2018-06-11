65ed16132b0cbe0685a17ed86b092362550f55f2
John Haney <geojwh@gmail.com>
2018-06-11 16:00:33 -0500

    almost done...

--- script.py
+++ script.py
@@ -3,21 +3,18 @@ this script will read markdown files from a directory, extract the front matter
 fields and values and write them to a csv - field names and values.
 '''
 
-import os, re, csv
+import os, csv
 
 def main():
 
     directory = os.getcwd() + "/md_files"
-    mdObject = {}
-    count = 0
-    bigList = []
-    fieldList = []
+    csv_file = os.getcwd() + "/test.csv"
     mdList = []
     lineArray = []
+    fieldList = []
+    count = 0
     master = {}
-    multi = []
-    unique = []
-    viewed = set()
+    dash = ''
 
     for subdir, dirs, files in os.walk(directory):
         for file in files:
@@ -28,63 +25,44 @@ def main():
                 f = open(file, 'r')
                 line = f.readline()
 
+                mdObject = {}
+
                 for line in f:
-                    if line.rstrip() == "---":
-                        break
-                    line = line.rstrip()
-                    #print(line)
+                    if line.strip() == "---":
+                        dash = str(dash + (count + 1))
+                        print(dash)
+                    line = line.strip()
                     s = line.find(':')
                     lineArray = line.split(line[s])
-                    #print(lineArray)
 
-                    #put the urls back together
+                    # put the urls back together
                     if len(lineArray) > 2:
                         url = ':'.join(lineArray[1:])
                         for x in lineArray[1:]:
                             lineArray.pop()
-                        print(lineArray)
                         lineArray.append(url)
 
-                    print(lineArray)
+                    mdObject[lineArray[0]] = lineArray[1]
 
-                    for i in lineArray:
-                        if i not in viewed:
-                            unique.append(i)
-                            viewed.add(i)
+                    # add functionality for scraping all text below '---' for
+                    # value of long description key
 
-                    print(unique)
 
-                f.close()
+                master[fileName] = mdObject
 
-            print(mdObject)
+    for k, v in master.items():
+        for item in v:
+            if item not in fieldList:
+                fieldList.append(item)
 
-    '''
-    for line in f:
-        if line.startswith("---"):
-            pass
-        elif line.startswith(""):
-            descript = "description"
-            dValue = line
-        else:
-            line = line.strip()
-            s = line.find(':')
-            if s != -1:
-                lineArray = line.split(line[s])
-        mdList.append(lineArray)
+    #add functionality for creating csv from master dictionary
+    with open(csv_file, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldList)
 
-    f.close()
-    '''
-    #create list of unique field names
-    '''
-    for x in mdList:
-        for i in x:
-            if i ==
-        fieldList.append(x[0])
-    '''
-    #print(mdList)
-    #print(len(fieldList))
-    #print(len(mdList))
-    #print(mdList(set(mdList)))
+        writer.writeheader()
 
-#run it
+        for k, v in master.items():
+            writer.writerow(v)
+
+#run it!
 main()
