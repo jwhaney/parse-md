a8bca539f1b265550f03ada32aa448284e5f8362
John Haney <geojwh@gmail.com>
2018-06-26 15:28:49 -0500

    delete archive folder and added .DS_Store to .gitignore

--- archive/output.py
+++
@@ -1,9 +0,0 @@
-# module to write stuff to csv - output
-
-# create csv from master dictionary and use unique field names from fieldList
-def output():
-    with open(csv_file, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldList)
-        writer.writeheader()
-        for k, v in master.items():
-            writer.writerow(v)
